af0732d1d46dcb6c01e2e452ff52a138e5a8ee5b
JustinLieu06 <justinlieu06@gmail.com>
2023-12-22 14:14:08 -0800

    completed all part 1 exercises

--- part1/1.6-1.14/src/App.jsx
+++ part1/1.6-1.14/src/App.jsx
@@ -39,6 +39,32 @@ const Button = ({onClick, text}) => {
 }
 
 const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  const anecdoteLikes = new Array(anecdotes.length).fill(0);
+   
+  const [selected, setSelected] = useState(0)
+  const [likes, setLikes] = useState(anecdoteLikes)
+  const [maxLikeIdx, setMaxLikeIdx] = useState(0);
+
+  console.log('maxLikeIdx: '+maxLikeIdx);
+
+  const handleClickLike = () => {
+    console.log('handle click like');
+    const copy = [...likes];
+    copy[selected]+=1;
+    if (copy[selected]>likes[maxLikeIdx]) setMaxLikeIdx(selected); // keep track of which idx has most likes
+    setLikes(copy);
+  }
+
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -46,7 +72,24 @@ const App = () => {
 
   return (
     <div>
-      <h3>Give Feedback: </h3>
+      <h3>Random Anecdote</h3>
+      <div>
+        {anecdotes[selected]}
+      </div>
+      <div>
+        <Button onClick={()=>setSelected(Math.floor(Math.random()*anecdotes.length))} text="Random Anecdote"></Button>
+        <Button onClick={handleClickLike} text="Like"></Button>
+      </div>
+      <div>
+        Likes: {likes[selected]}
+      </div>
+
+      <h3>Anecdote with most votes</h3>
+      <div>
+        {anecdotes[maxLikeIdx]}
+      </div>
+
+      <h3>Give Feedback of this App: </h3>
       <div>
         <Button onClick={()=>setGood(good+1)} text="Good" />
         <Button onClick={()=>setNeutral(neutral+1)} text="Neutral" />
