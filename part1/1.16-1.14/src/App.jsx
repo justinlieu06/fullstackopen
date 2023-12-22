8e1b53ae1a6d18c65c6d625512667b6a14fe91fc
JustinLieu06 <justinlieu06@gmail.com>
2023-12-22 10:57:32 -0800

    completed 1.10

--- part1/1.16-1.14/src/App.jsx
+++ part1/1.16-1.14/src/App.jsx
@@ -1,5 +1,40 @@
 import { useState } from 'react'
 
+const StatisticLine = ({text, value}) => {
+  return (
+    <p>
+      {text} {value}
+    </p>
+  )
+}
+
+const Statistics = (props) => {
+  console.log(props);
+  const feedbackGiven = (props.good || props.neutral || props.bad);
+  console.log(feedbackGiven);
+  const defaultMessage = "No feedback given";
+  return (
+    <div>
+      <h3>Statistics:</h3>
+      {feedbackGiven ? <div>
+        <StatisticLine text="Good Counter: " value={props.good} />
+        <StatisticLine text="Neutral Counter: " value={props.neutral} />
+        <StatisticLine text="Bad Counter: " value={props.bad} />
+        <StatisticLine text="Total: " value={((props.good-props.bad)/(props.good+props.neutral+props.bad)).toFixed(4)} />
+        <StatisticLine text="Positive: " value={(props.good/(props.good+props.neutral+props.bad)).toFixed(4)} />
+        </div> :
+        defaultMessage
+      }
+    </div>
+  )
+}
+
+const Button = ({onClick, text}) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -10,17 +45,13 @@ const App = () => {
     <div>
       <h3>Give Feedback: </h3>
       <div>
-        <button onClick={()=>setGood(good+1)}>Good</button>
-        <button onClick={()=>setNeutral(neutral+1)}>Neutral</button>
-        <button onClick={()=>setBad(bad+1)}>Bad</button>
+        <Button onClick={()=>setGood(good+1)} text="Good" />
+        <Button onClick={()=>setNeutral(neutral+1)} text="Neutral" />
+        <Button onClick={()=>setBad(bad+1)} text="Bad" />
+        
       </div>
-      <h3>Statistics:</h3>
-      <p>Good Counter: {good}</p>
-      <p>Neutral Counter: {neutral}</p>
-      <p>Bad Counter: {bad}</p>
-      <p>Total: {good+neutral+bad}</p>
-      <p>Average: {((good-bad)/(good+neutral+bad)).toFixed(4)}</p>
-      <p>Positive: {(good/(good+neutral+bad)).toFixed(4)}</p>
+      
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
